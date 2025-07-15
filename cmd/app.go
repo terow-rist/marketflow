270cef1873febb23e8c8105411b0e54e1701b033
missayev <feeerhd.342@gmail.com>
2025-07-15 17:53:45 +0500

    update: pull exchanges from docker hub + simple data acqusition

--- cmd/app.go
+++ cmd/app.go
@@ -1,18 +1,17 @@
 package cmd
 
 import (
-	"fmt"
+	"bufio"
 	"log"
 	"log/slog"
 	"marketflow/internal/adapter/config"
 	flag "marketflow/internal/adapter/config"
-	httpserver "marketflow/internal/adapter/handler/http"
 	"marketflow/internal/adapter/logger"
-	"net/http"
+	"net"
+	"os"
 )
 
 func Run() {
-
 	// Parse flags
 	err := flag.Parse()
 	if err != nil {
@@ -25,14 +24,36 @@ func Run() {
 	//Set logger
 	logger.Set()
 	slog.Info("Staring application", "app", config.App.Name, "env", config.App.Env)
+	tempCatch()
 
-	//Futere init db
+	// //Futere init db
 
-	//Server setup
-	temp := httpserver.NewTemp()
+	// //Server setup
+	// temp := httpserver.NewTemp()
 
-	mux := httpserver.NewRouter(temp)
-	slog.Info(fmt.Sprintf("Listening on port: %d", flag.Port))
-	err = http.ListenAndServe(fmt.Sprintf(":%d", flag.Port), mux)
-	log.Fatal(err)
+	// mux := httpserver.NewRouter(temp)
+	// slog.Info(fmt.Sprintf("Listening on port: %d", flag.Port))
+	// err = http.ListenAndServe(fmt.Sprintf(":%d", flag.Port), mux)
+	// log.Fatal(err)
+}
+
+func tempCatch() {
+	conn, err := net.Dial("tcp", "exchange1:40101") // Connect to exchange1
+	if err != nil {
+		slog.Error("Failed to connect to exchange1", "error", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	slog.Info("Connected to exchange1")
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		msg := scanner.Text()
+		slog.Info("Price update", "data", msg)
+	}
+
+	if err := scanner.Err(); err != nil {
+		slog.Error("Error reading from exchange1", "error", err)
+	}
 }
--- load.sh
+++
@@ -1,3 +0,0 @@
-docker load -i ./exchanges/exchange1_amd64.tar
-docker load -i ./exchanges/exchange2_amd64.tar
-docker load -i ./exchanges/exchange3_amd64.tar
